ff7e001f77f5cc651ec43ba886c65b32e6a930aa
Chase Johnson <chaseajohnson@gmail.com>
2014-07-28 15:38:51 +1000

    Update to C/Cpp sublime projects. Fixed issue with base directory. Updated makefiles.

--- EmbeddedC/src/HelloWorldC++.c
+++
@@ -1,9 +0,0 @@
-// my first program in C
-#include <stdio.h>
-
-int main()
-{
-  printf("Hello World!");
-
- return 0;
-}
---
+++ EmbeddedC/src/main/HelloWorld.c
@@ -0,0 +1,9 @@
+// my first program in C
+#include <stdio.h>
+
+int main()
+{
+  printf("Hello World!");
+
+ return 0;
+}
